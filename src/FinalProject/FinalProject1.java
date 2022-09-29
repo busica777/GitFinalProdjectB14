435b507b209a7806a232ebd11244ba9286b14891
busica777 <gotruck.312@gmail.com>
2022-09-28 19:48:29 -0500

    second commit

--- src/FinalProject/FinalProject1.java
+++ src/FinalProject/FinalProject1.java
@@ -1,26 +1,10 @@
 package FinalProject;
 
-public class FinalClass {
+public class FinalProject1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("This is the code for mercedes");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+System.out.println("this is the code for the audi");
 	}
 
 }
